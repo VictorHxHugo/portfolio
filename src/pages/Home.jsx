54d3fabc2182c1ec637565b161d02e83c56639f1
VictorHxHugo <victorzk6@hotmail.com>
2025-05-28 05:54:15 -0300

    TailwindCSS Setup and Theme Customization

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+export const Home = () => {
+  return (
+    <div className="min-h-screen">
+      {/* Theme toggle */}
+      {/* BackGround Effects */}
+      {/* Navbar */}
+      {/* Main Content */}
+      {/* Footer */}
+    </div>
+  );
+};
