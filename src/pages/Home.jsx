d2ad655b555744d1b330e07f91a3cf746bb24b5b
VictorHxHugo <victorzk6@hotmail.com>
2025-05-29 09:00:19 -0300

    Theme Toggle and Background Effects

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,13 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "@/components/StarBackground";
+
 export const Home = () => {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       {/* Theme toggle */}
+      <ThemeToggle />
       {/* BackGround Effects */}
+      <StarBackground />
       {/* Navbar */}
       {/* Main Content */}
       {/* Footer */}
