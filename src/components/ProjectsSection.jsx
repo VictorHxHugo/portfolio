6d0f8a50cec3078257e9cb35e4f758349d5a3b91
VictorHxHugo <victorzk6@hotmail.com>
2025-09-05 11:08:50 -0300

    Setting Project Sections

---
+++ src/components/ProjectsSection.jsx
@@ -0,0 +1,14 @@
+const projects = [
+  {
+    id: 1,
+    title: "",
+    description: "",
+    image: "/projects/project1.png",
+    tags: [],
+    githubUrl: "#",
+  },
+];
+
+export const ProjectsSection = () => {
+  return <section></section>;
+};
